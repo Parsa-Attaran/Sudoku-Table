4f2ba25c91fa34b338d5492d2b53132dbd8a70ee
Parsa Attaran <107747449+Parsa-Attaran@users.noreply.github.com>
2025-01-30 22:19:19 +0330

    Add files via upload

---
+++ sudoku.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <time.h>
+#include "clui.h"
+#include "function.h"
+using namespace std;
+
+int main() {
+    clear_screen();
+    delay(400);
+    welcom_page();
+    getch();
+    return 0;
+}
